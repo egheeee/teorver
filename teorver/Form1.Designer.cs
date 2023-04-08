8ec2e2e961dfe1f85451ba1c859cfd08aac7079e
Zalman <Zalman@DESKTOP-04IJ2U4>
2023-04-08 14:39:52 +0700

    Добавьте файлы проекта.

---
+++ teorver/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace teorver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(89, 119);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(135, 72);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Равномерное распределение";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(89, 242);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(135, 72);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Нормальное распределение";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // chart2
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart2.Legends.Add(legend1);
+            this.chart2.Location = new System.Drawing.Point(338, 12);
+            this.chart2.Name = "chart2";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart2.Series.Add(series1);
+            this.chart2.Size = new System.Drawing.Size(495, 449);
+            this.chart2.TabIndex = 2;
+            this.chart2.Text = "chart2";
+            this.chart2.Click += new System.EventHandler(this.chart2_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(867, 491);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+    }
+}
+
---
+++ teorver/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace teorver
+{
+    public partial class Form1 : Form
+    {
+        int N = 1000;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var uniformData = GenerateUniformDistribution(N);
+            DrawHistogram(uniformData);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var normalData = GenerateNormalDistribution(N);
+            DrawHistogram(normalData);
+        }
+
+        private void chart2_Click(object sender, EventArgs e)
+        {
+            chart2.Series[0].Points.Clear();
+        }
+        private List<double> GenerateUniformDistribution(int count)
+        {
+            Random r = new Random();
+            List<double> uniformData = new List<double>();
+
+            for (int i = 0; i < count; i++)
+            {
+                uniformData.Add(r.NextDouble());
+            }
+
+            return uniformData;
+        }
+        private void DrawHistogram(List<double> data)
+        {
+            int numberOfBins = 10; // Выберите количество интервалов для гистограммы
+            double[] frequencies = new double[numberOfBins];
+            double binWidth = 1.0 / numberOfBins;
+
+            foreach (double value in data)
+            {
+                int binIndex = (int)(value / binWidth);
+                frequencies[binIndex]++;
+            }
+            chart2.Series[0].Points.Clear();
+            for (int i = 0; i < numberOfBins; i++)
+            {
+                double x = i * binWidth + binWidth / 2.0;
+                double y = frequencies[i] / (data.Count * binWidth);
+                chart2.Series[0].Points.AddXY(x, y);
+            }
+        }
+        private List<double> GenerateNormalDistribution(int count)
+        {
+            Random r = new Random();
+            List<double> normalData = new List<double>();
+            int n = 20;
+
+            for (int i = 0; i < count; i++)
+            {
+                double sum = 0;
+
+                for (int j = 0; j < n; j++)
+                {
+                    sum += r.NextDouble();
+                }
+
+                double normalValue = sum / n;
+                normalData.Add(normalValue);
+            }
+
+            return normalData;
+        }
+    }
+}
