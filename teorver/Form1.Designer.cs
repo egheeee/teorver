2c257db08c7d3104a537d38af9c906cf2c260509
egheeee <125895243+egheeee@users.noreply.github.com>
2023-04-08 14:57:12 +0700

    Update Form1.Designer.cs

--- teorver/Form1.Designer.cs
+++ teorver/Form1.Designer.cs
@@ -89,13 +89,7 @@ namespace teorver
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+       
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
